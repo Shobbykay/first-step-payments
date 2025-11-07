64c08ef375de0b1219c86874728be835ea06c19c
shobbykay <shobbykay.oo@gmail.com>
2025-11-07 14:33:37 +0100

    fix

--- controllers/userTicketController.js
+++ controllers/userTicketController.js
@@ -22,7 +22,8 @@ const upload = multer({
   storage,
   limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
   fileFilter: (req, file, cb) => {
-    const filetypes = /jpeg|jpg|png/;
+    // const filetypes = /jpeg|jpg|png/;
+    const filetypes = /\.(jpeg|jpg|png)$/i;
     const ext = path.extname(file.originalname).toLowerCase();
     const mimetype = file.mimetype;
 
