86a8aef58e9298d1a27c181dd1d8543b30eeb65d
shobbykay <shobbykay.oo@gmail.com>
2025-11-15 19:18:06 +0100

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -152,7 +152,7 @@ exports.verifyOtp = async (req, res) => {
       // Generate JWT token
       const token = jwt.sign(
         { user_id: user.user_id, phone_number: user.phone_number },
-        process.env.JWT_SECRET || "your_jwt_secret",
+        process.env.JWT_SECRET,
         { expiresIn: "7d" } // token validity (7 days)
       );
   
